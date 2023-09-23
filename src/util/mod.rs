547982ffa4c0ca819d700a795a2962effcd9826d
cryeprecision <muench.rico@gmail.com>
2023-09-23 23:24:39 +0200

    remove rate limit module

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,8 @@
 #[cfg(feature = "friend_code")]
 pub mod bit_chunks;
 
-mod rate_limit;
-pub use rate_limit::*;
+// mod rate_limit;
+// pub use rate_limit::*;
 
 mod visibility;
 pub use visibility::Visibility;
